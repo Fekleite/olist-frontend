fc9c91074c9ba6b5cc69241c62e14784b73d0874
Fernanda Leite <fernandacarolineleite360@gmail.com>
2020-09-05 16:21:49 -0300

    feat: Create text balloon component

---
+++ src/components/TextBalloon/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+function TextBalloon({ text }) {
+  return <Container>{text}</Container>;
+}
+
+export default TextBalloon;
---
+++ src/components/TextBalloon/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: auto;
+  background: var(--color-light-blue02);
+  padding: 1.6rem;
+  font-size: 1.4rem;
+  line-height: 1.7rem;
+  display: flex;
+  align-items: center;
+  border-radius: 4px;
+`;
